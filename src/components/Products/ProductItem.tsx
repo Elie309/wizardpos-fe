18550acd858445ee9ffeae95831c8d74d2acc9d6
Elie309 <elie.saade432@gmail.com>
2024-11-05 18:20:47 +0200

    Enhance ProductItem and ProductMenu components with category filtering and improved layout

--- src/components/Products/ProductItem.tsx
+++ src/components/Products/ProductItem.tsx
@@ -8,9 +8,13 @@ interface ProductItemProps {
 
 export default function ProductItem({ product }: ProductItemProps) {
     return (
-        <div className="flex justify-between">
-            <p>{product.name}</p>
-            <p>${product.price.toFixed(2)}</p>
+
+        <div className="bg-white shadow-lg w-36 h-36 rounded-lg">
+            <img src={product.image} alt={product.name} className="object-cover rounded-t-lg" />
+
+            <div className="px-2">
+                <p className="my-2 text-sm">{product.name}</p>
+            </div>
         </div>
     );
 }
--- src/components/Products/ProductMenu.tsx
+++ src/components/Products/ProductMenu.tsx
@@ -3,30 +3,49 @@ import ProductService from "../../services/ProductService";
 import Product from "../../types/Product";
 import ProductItem from "./ProductItem";
 
-export default function ProductMenu(){
+export default function ProductMenu() {
 
     const [products, setProducts] = useState<Product[]>([]);
     const [loading, setLoading] = useState(true);
+    const [category, setCategory] = useState(['All']);
+    const [selectedCategory, setSelectedCategory] = useState('All');
+    const [initialProduct, setInitialProduct] = useState<Product[]>([]);
+
 
     //get menu products
     const getMenuProducts = async () => {
-        try{
+        try {
 
             const response = await ProductService.getMenuProducts();
             setProducts(response);
+            setInitialProduct(response);
             setLoading(false);
 
-        }catch(error){
+        } catch (error) {
             setLoading(false);
         }
     }
+
+    const handleCategoryFilter = (e: any) => {
+        setSelectedCategory(e.target.innerText);
+        if (e.target.innerText === 'All') {
+            setProducts(initialProduct);
+        } else {
+           
+            setProducts(initialProduct.filter((product) => product.categoryName === e.target.innerText));
+
+        }
+
+    }
+
 
     useEffect(() => {
         setLoading(true);
         getMenuProducts();
     }, [])
 
-    if(loading){
+
+    if (loading) {
         return (
             <div className="w-full h-full flex justify-center items-center ">
                 <div className="loader scale-150"></div>
@@ -35,10 +54,32 @@ export default function ProductMenu(){
     }
 
     return (
-        <div>
-            {products.map((product: Product) => (
-                <ProductItem product={product} key={product.id} />
-            ))}
+        <div className="relative mx-8 flex flex-col overflow-y-scroll overflow-x-hidden w-full h-5/6">
+
+            <div className="h-fit">
+                {category.map((cat) => {
+                    return <button key={cat}
+                        className={`${selectedCategory === cat ? "bg-blue-300" : "bg-gray-200"} 
+                            text-gray-700 px-2 py-1 m-2 rounded-lg hover:bg-blue-300 sm:text-md md:text-lg`}
+                        onClick={handleCategoryFilter}
+                        >
+                        {cat}
+                    </button>
+                })}
+
+            </div>
+            <div className="w-full h-full flex flex-row flex-wrap gap-4">
+            {products.map((product: Product) => {
+
+                if (!category.includes(product.categoryName)) {
+                    setCategory([...category, product.categoryName])
+                }
+
+                return <ProductItem key={product.id} product={product} />
+            })}
+            </div>
+
+
         </div>
     )
 
