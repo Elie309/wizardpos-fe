59490eb9ccba0ba6af832bddd070b7c4a0943658
Elie309 <elie.saade432@gmail.com>
2024-11-15 00:23:50 +0200

    Refactor ErrorPage component to use named export and improve readability

--- src/pages/Others/ErrorPage.tsx
+++ src/pages/Others/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const ErrorPage = () => {
+export default function ErrorPage () {
     return (
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="text-center">
@@ -19,5 +19,3 @@ const ErrorPage = () => {
         </div>
     );
 };
-
-export default ErrorPage;
