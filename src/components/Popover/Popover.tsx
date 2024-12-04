d58be3864408425a83ad1ea235194f73d8919e4d
Elie309 <elie.saade432@gmail.com>
2024-12-05 00:46:03 +0200

    Create Popover component to use forwardRef and improve toggle functionality with custom button and title props

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { forwardRef, useImperativeHandle } from 'react';;
 
 interface PopoverProps {
   id: string;
   children: React.ReactNode;
+  buttonName: string;
+  title: string
+  classNameButton: string;
+  classNameMainDiv: string;
 }
 
-export default function Popover({ id, children }: PopoverProps) {
+const Popover = forwardRef((props: PopoverProps, ref) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleOnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  }
+
+  const open = () => {
+    setIsOpen(true);
+  }
+
+
+  useImperativeHandle(ref, () => ({
+    open, close
+  }));
+
   return (
-    <div className="relative">
-      <button className="popover-button" aria-describedby={id}>
-        Toggle Popover
-      </button>
-      <div id={id} role="tooltip" className="popover-content absolute bg-white p-4 shadow-lg rounded">
-        {children}
-      </div>
+    <div>
+      <a className={" cursor-pointer "+ props.classNameButton} onClick={handleOnClick} aria-describedby={props.id}>
+        {props.buttonName}
+      </a>
+      {isOpen &&
+        <div className={`${isOpen ? "" : "hidden"} w-screen h-screen  
+          fixed inset-0 bg-black bg-opacity-50 
+          flex justify-start items-start md:items-center md:justify-center z-50
+          `}>
+
+          <div id={props.id} role="tooltip" className={`absolute w-full h-full md:h-fit md:w-3/4
+            bg-white p-2 shadow-lg rounded ${props.classNameMainDiv}`}>
+
+            <div className='flex flex-row justify-end pr-4'>
+              <h1 className='secondary-title'>{props.title}</h1>
+              <button onClick={() => setIsOpen(false)}
+                className='text-red-800 text-xl'>
+                X
+              </button>
+            </div>
+
+            
+            {props.children}
+          </div>
+
+
+        </div>}
+
     </div>
   );
-}
+});
+
+export default Popover;
