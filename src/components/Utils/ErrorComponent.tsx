81d50ddaaf5d1c3a8ccffaf785625138feaa5547
Elie309 <elie.saade432@gmail.com>
2024-12-10 00:12:51 +0200

    Refactor ErrorDisplay component to simplify message rendering and improve readability

--- src/components/Utils/ErrorComponent.tsx
+++ src/components/Utils/ErrorComponent.tsx
@@ -1,7 +1,7 @@
 export default function ErrorDisplay({ message }: { message: string | null }) {
     return (
-        <div className="p-4 rounded-md flex justify-center items-center border border-red-600 bg-red-100 ">
-            <div className="text-red-600 text-base">{message ?? "An Error occurred"}</div>
+        <div className="p-4 rounded-md flex justify-center items-center border border-red-600 text-red-600 bg-red-100 ">
+            {message ?? "An Error occurred"}
         </div>
 
     )
